d4a693e2d6d783e39e530463b7567e1e4015f132
yoda1202 <esforcos_unidad@ezweb.ne.jp>
2023-10-27 19:49:30 +0900

    create WEBAPI

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/todos")] // このコントローラーのルートURLを指定します
+[ApiController]
+public class TodoController : ControllerBase
+{
+    // ここに後ほど取得機能のアクションメソッドを追加します
+}
